83030fb9b4731f0bf0b96e99d6dc5fff0186f28e
Roman Kuite <roman.kuite@gmail.com>
2021-01-27 10:57:27 +0100

    Add animals with inheritance of the animal class

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,24 @@
+package animals;
+//
+public class Animal {
+    private int oxygenLevel;
+    private int bodyWeightKG;
+    // Default Constructor
+    public Animal(){
+        this.oxygenLevel = 5;
+    }
+    //Constructor
+    public Animal (int bodySize){
+        this.oxygenLevel = 5;
+        this.bodyWeightKG = bodyWeightKG;
+    }
+    //
+    public void breathe (){
+        System.out.println("Breathe in - Breathe out!");
+        oxygenLevel += 10;
+    }
+
+    public boolean isAlive() {
+        return oxygenLevel > 0;
+    }
+}
